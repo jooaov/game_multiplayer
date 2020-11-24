ff97ecd3f46d1615cdac024ccf4505411176a161
jooaov <jjoao.jv13@gmail.com>
2020-11-23 21:14:47 -0300

    add colision in fruits and players

--- playground/1st-release/inputs.js
+++ playground/1st-release/inputs.js
@@ -1,32 +1,35 @@
 
-        document.addEventListener('keydown',handleKeydown);
+function createKeybordListener() {
+    const state = {
+        observers: []
+    }
 
-        function handleKeydown(event) {
-            const keyPressed =event.key;
-            const player = game.players[currentPlayerId];
+    function subscribe(observerFuncition) {
+        state.observers.push(observerFuncition);
+    }
 
-            if (keyPressed === 'ArrowUp') {
-                player.y=player.y-1;
-            }else
-            if (keyPressed === 'ArrowRight') {
-                player.x=player.x+1;
-            }
-            if (keyPressed === 'ArrowDown') {
-                player.y=player.y+1;
-            }else
-            if (keyPressed === 'ArrowLeft') {
-                player.x=player.x-1;
-            }
+    //executa a função moveplayer com o parametro comando
+    function notifyAll(command) {
+        console.log(`Notifying ${state.observers.length} observers`);
 
-            if(player.x > 9){
-                player.x = 0;
-            }else if(player.x < 0){
-                player.x=9;
-            }
-            
-            if(player.y > 9){
-                player.y = 0;
-            }else if(player.y < 0){
-                player.y=9;
-            }
+        for (const observerFuncition of state.observers) {
+            observerFuncition(command);
+
         }
+    }
+    document.addEventListener('keydown', handleKeydown);
+
+    function handleKeydown(event) {
+        const keyPressed = event.key;
+        //comando que é enviado a função
+        const command = {
+            playerId: 'player1',
+            keyPressed
+        }
+        notifyAll(command);
+
+    }
+    return {
+        subscribe
+    }
+}
--- playground/1st-release/render.js
+++ playground/1st-release/render.js
@@ -1,33 +1,18 @@
- //pega o canvas e seta o context para 2d
- const screen = document.getElementById('screen');
- const context = screen.getContext('2d');
- const currentPlayerId = 'player1';
+function rederScreen() {
+    context.fillStyle = 'white';
+    //pega o total do canvas e limpa
+    context.clearRect(0, 0, 10, 10);
 
- const game = {
-     players: {
-         'player1': { x: 1, y: 1 },
-         'player2': { x: 9, y: 9 }
-     },
-     'fruits': {
-         'fruit1': { x: 3, y: 1 }
-     }
- }
-
- rederScreen()
- function rederScreen() {
-     context.fillStyle='white';
-     //pega o total do canvas e limpa
-     context.clearRect(0,0,10,10);
-
-     for (const playerId in game.players) {
-         const player = game.players[playerId];
-         context.fillStyle = 'black';
-         context.fillRect(player.x, player.y, 1, 1);
-     }
-     for (const fruitsId in game.fruits) {
-         const fruit = game.fruits[fruitsId];
-         context.fillStyle = 'green';
-         context.fillRect(fruit.x, fruit.y, 1, 1);
-     }
-     requestAnimationFrame(rederScreen);
- }
+    for (const playerId in game.state.players) {
+        const player = game.state.players[playerId];
+        //teste
+        context.fillStyle = 'black';
+        context.fillRect(player.x, player.y, 1, 1);
+    }
+    for (const fruitsId in game.state.fruits) {
+        const fruit = game.state.fruits[fruitsId];
+        context.fillStyle = 'green';
+        context.fillRect(fruit.x, fruit.y, 1, 1);
+    }
+    requestAnimationFrame(rederScreen);
+}
